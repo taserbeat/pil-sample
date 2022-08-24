1242dc08a1288302dd9dbb923a972464f6031e58
taserbeat <taserbeat@gmail.com>
2022-08-24 20:53:28 +0900

    サンプルコードを作成

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 import os
+import numpy as np
+from PIL import Image, ImageDraw
+from matplotlib import pyplot as plt
 
-print('Success to create python project from template, Nice Job!!\n')
+# 各種画像の変換
+# https://qiita.com/mo256man/items/8912934578025adb5ce5
 
-print('Now, this project has below file/directory in root.')
-print('====================================================')
-filenames = os.listdir('.')
-for filename in filenames:
-    print(filename)
-print('====================================================')
+IMAGES_DIR = os.path.join(os.getcwd(), "images")
+IMAGE_FILE_NAME = "space.jpg"
+
+image_file_path = os.path.join(IMAGES_DIR, IMAGE_FILE_NAME)
+
+# PILで画像読み込み
+pil_image = Image.open(image_file_path)
+print(type(pil_image))  # <class 'PIL.JpegImagePlugin.JpegImageFile'>
+
+# 画像描画の準備
+drawer = ImageDraw.Draw(pil_image)
+
+# 矩形の描画
+x_min, y_min, x_max, y_max = 100, 50, 300, 200
+drawer.rectangle(
+    ((x_min, y_min), (x_max, y_max)),
+    outline=(255, 0, 0)
+)
+
+# 文字の描画
+drawer.text(
+    (x_min, y_min - 10), "abcABC", (32, 196, 72)
+)
+
+# numpy型に変換
+np_image = np.asarray(pil_image)
+print(type(np_image))  # <class 'numpy.ndarray'>
+
+# 画像表示
+plt.imshow(np_image)
+plt.show()
